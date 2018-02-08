40e34224635216514c9f91d10784ada0f044efee
Zheng <yuanzhengstl@gmail.com>
2018-02-08 10:06:19 -0800

    add user authentication configuration

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,14 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+-- noinspection SqlDialectInspectionForFile
+
+-- Insert role
+INSERT INTO role (name) VALUES ('ROLE_USER');
+
+-- Insert two users
+INSERT INTO user (username, enabled, password, role_id) VALUES ('user', true, 'password', 1);
+INSERT INTO user (username, enabled, password, role_id) VALUES ('user2', true, 'password', 1);
+
 -- Insert tasks
 insert into task (complete,description) values (true,'Code Task entity');
 insert into task (complete,description) values (false,'Discuss users and roles');
